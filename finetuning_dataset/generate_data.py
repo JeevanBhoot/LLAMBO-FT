4e6cd289109664e0e7e46e7c9d6214778b3a6277
JeevanBhoot <jeevan.bhoot@yahoo.com>
2024-10-27 13:47:41 +0000

    Run each {model, dataset} pair N(=5) times

--- finetuning_dataset/generate_data.py
+++ finetuning_dataset/generate_data.py
@@ -76,10 +76,9 @@ def hyps_to_int(config: dict):
     config['max_depth'] = int(config['max_depth'])
     config['min_samples_split'] = int(config['min_samples_split'])
     config['min_samples_leaf'] = int(config['min_samples_leaf'])
-    return config
 
 
-def generate_training_data(model_name, dataset_id, dataset_name, n_trials=25, n_initial_points=5):
+def generate_training_data(model_name, dataset_id, dataset_name, num_expts=5, n_trials=25, n_initial_points=5):
     # Get dataset information from OpenML
     dataset_info = get_dataset_info(dataset_id)
 
@@ -89,87 +88,91 @@ def generate_training_data(model_name, dataset_id, dataset_name, n_trials=25, n_
     # Define search space based on model
     pbounds = HYPERPARAM_BOUNDS[model_name]
 
-    # Define Bayesian Optimization process
-    bo = BayesianOptimization(
-        f=lambda **params: benchmark.objective_function(
-            {
-                **params, 
-                "max_depth": int(params["max_depth"]),
-                "min_samples_split": int(params["min_samples_split"]),
-                "min_samples_leaf": int(params["min_samples_leaf"])
+    for j in range(num_expts):
+        os.makedirs(f"{data_dir}/{dataset_name}/{model_name}/exp0{j+1}", exist_ok=True)
+        # Define Bayesian Optimization process
+        bo = BayesianOptimization(
+            f=lambda **params: benchmark.objective_function(
+                {
+                    **params, 
+                    "max_depth": int(params["max_depth"]),
+                    "min_samples_split": int(params["min_samples_split"]),
+                    "min_samples_leaf": int(params["min_samples_leaf"])
+                }
+            )["info"]["val_scores"]["acc"],
+            pbounds=pbounds,
+            verbose=2,
+        )
+
+        # Run Bayesian Optimization with initial points and trials
+        bo.maximize(init_points=n_initial_points, n_iter=n_trials)
+
+        optimization_history = []
+        # Initial steps handled differently to future trials
+        for i in range(n_initial_points):
+            trial = bo.res[i]
+            config = trial["params"]
+            hyps_to_int(config)
+            result = trial["target"]
+
+            bal_acc, f1, precision = get_metrics(benchmark, config)
+
+            optimization_history.append({
+                "hyperparameters": config,
+                "performance": {
+                    "accuracy": result,
+                    "balanced_accuracy": bal_acc,
+                    "f1_score": f1,
+                    "precision": precision
+                }
+            })
+
+            step_data = {
+                "model_name": MODEL_NAME_MAP[model_name],
+                "task": "Classification",  # hpo_bench datasets are all classification
+                "metric": "accuracy",
+                "dataset_info": dataset_info,
+                "current_step": 0,
+                "optimization_history": [], # empty for initial steps
+                "current_hyperparameters": config,
+                "current_performance": optimization_history[i]["performance"],
             }
-        )["info"]["val_scores"]["acc"],
-        pbounds=pbounds,
-        verbose=2,
-    )
+            with open(f"{data_dir}/{dataset_name}/{model_name}/exp0{j+1}/initial_step_{i}.json", "w") as f:
+                json.dump(step_data, f, indent=4)
 
-    # Run Bayesian Optimization with initial points and trials
-    bo.maximize(init_points=n_initial_points, n_iter=n_trials)
+        # Save jsons for steps after initial points
+        for i in range(n_initial_points, len(bo.res)):
+            trial = bo.res[i]
+            config = trial["params"]
+            hyps_to_int(config)
+            result = trial["target"]
 
-    optimization_history = []
-    # Initial steps handled differently to future trials
-    for i in range(n_initial_points):
-        trial = bo.res[i]
-        config = hyps_to_int(trial["params"])
-        result = trial["target"]
+            bal_acc, f1, precision = get_metrics(benchmark, config)
 
-        bal_acc, f1, precision = get_metrics(benchmark, config)
-
-        optimization_history.append({
-            "hyperparameters": config,
-            "performance": {
-                "accuracy": result,
-                "balanced_accuracy": bal_acc,
-                "f1_score": f1,
-                "precision": precision
+            optimization_history.append({
+                "hyperparameters": config,
+                "performance": {
+                    "accuracy": result,
+                    "balanced_accuracy": bal_acc,
+                    "f1_score": f1,
+                    "precision": precision
+                }
+            })
+            step_num = i + 1 - n_initial_points
+            step_data = {
+                "model_name": MODEL_NAME_MAP[model_name],
+                "task": "Classification",
+                "metric": "accuracy",
+                "dataset_info": dataset_info,
+                "current_step": step_num,
+                "optimization_history": optimization_history[:i],  # Include all previous steps up to current
+                "current_hyperparameters": config,
+                "current_performance": optimization_history[i]["performance"],
             }
-        })
 
-        step_data = {
-            "model_name": MODEL_NAME_MAP[model_name],
-            "task": "Classification",  # hpo_bench datasets are all classification
-            "metric": "accuracy",
-            "dataset_info": dataset_info,
-            "current_step": 0,
-            "optimization_history": [], # empty for initial steps
-            "current_hyperparameters": config,
-            "current_performance": optimization_history[i]["performance"],
-        }
-        with open(f"{data_dir}/{dataset_name}/{model_name}/initial_step_{i}.json", "w") as f:
-            json.dump(step_data, f, indent=4)
-
-
-    for i in range(n_initial_points, len(bo.res)):
-        trial = bo.res[i]
-        config = hyps_to_int(trial["params"])
-        result = trial["target"]
-
-        bal_acc, f1, precision = get_metrics(benchmark, config)
-
-        optimization_history.append({
-            "hyperparameters": config,
-            "performance": {
-                "accuracy": result,
-                "balanced_accuracy": bal_acc,
-                "f1_score": f1,
-                "precision": precision
-            }
-        })
-        step_num = i +1 - n_initial_points
-        step_data = {
-            "model_name": MODEL_NAME_MAP[model_name],
-            "task": "Classification",
-            "metric": "accuracy",
-            "dataset_info": dataset_info,
-            "current_step": step_num,
-            "optimization_history": optimization_history[:i],  # Include all previous steps up to current
-            "current_hyperparameters": config,
-            "current_performance": optimization_history[i]["performance"],
-        }
-
-        # Save generated data to JSON file for each step
-        with open(f"{data_dir}/{dataset_name}/{model_name}/step_{step_num}.json", "w") as f:
-            json.dump(step_data, f, indent=4)
+            # Save generated data to JSON file for each step
+            with open(f"{data_dir}/{dataset_name}/{model_name}/exp0{j+1}/step_{step_num}.json", "w") as f:
+                json.dump(step_data, f, indent=4)
 
 # Run for all models and datasets
 for model in MODEL_BENCHMARK_MAP.keys():
